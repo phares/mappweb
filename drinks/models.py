c45022cff9be1eb09d42535b7b00dd3de40b8767
Phares <abungaphares@gmail.com>
2017-04-25 01:49:43 +0300

    added drinks model

--- drinks/models.py
+++ drinks/models.py
@@ -3,4 +3,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+brands = ('whisky')
+
 # Create your models here.
+class drinks(models.model):
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=20, blank=False,default='')
+    price = models.DecimalField()
+    available = models.BooleanField(default=True)
+    quantity = models.IntegerFiels()
+    brand = models.CharField(choices=brands, default='whisky', max_length=100)
+
+    class Meta:
+        ordering = ('created',)
