aea7c51be6eaeef5a332186ffd6bed2c54632cfb
Phares <abungaphares@gmail.com>
2017-04-25 06:14:25 +0300

    added generic views

--- drinks/urls.py
+++ drinks/urls.py
@@ -4,8 +4,11 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
 
-    url(r'^drinks/$', views.drink_list),
-    url(r'^drinks/(?P<pk>[0-9]+)/$', views.drink_detail),
+    # url(r'^drinks/$', views.drink_list),
+    # url(r'^drinks/(?P<pk>[0-9]+)/$', views.drink_detail),
+
+    url(r'^drinks/$', views.DrinkList.as_view()),
+    url(r'^drinks/(?P<pk>[0-9]+)/$', views.DrinkDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- drinks/views.py
+++ drinks/views.py
@@ -7,13 +7,17 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 from drinks.models import Drink
+from django.http import Http404
 from rest_framework import status
+from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from drinks.serializers import DrinkSerializer
+from rest_framework import mixins
+from rest_framework import generics
 
 # Create your views here.
-
+'''
 @api_view(['GET', 'POST'])
 def drink_list(request, format=None):
     """
@@ -55,3 +59,88 @@ def drink_detail(request, pk, format=None):
     elif request.method == 'DELETE':
         drink.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+'''
+
+'''
+class DrinkList(APIView):
+    """
+    List all drinks, or create a new drink.
+    """
+    def get(self, request, format=None):
+        snippets = Drink.objects.all()
+        serializer = DrinkSerializer(snippets, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = DrinkSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class DrinkDetail(APIView):
+    """
+    Retrieve, update or delete a drink instance.
+    """
+    def get_object(self, pk):
+        try:
+            return Drink.objects.get(pk=pk)
+        except Drink.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        drink = self.get_object(pk)
+        serializer = DrinkSerializer(drink)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        drink = self.get_object(pk)
+        serializer = DrinkSerializer(drink, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        drink = self.get_object(pk)
+        drink.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+'''
+'''
+class DrinkList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+    queryset = Drink.objects.all()
+    serializer_class = DrinkSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+class DrinkDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
+    queryset = Drink.objects.all()
+    serializer_class = DrinkSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+'''
+
+class DrinkList(generics.ListCreateAPIView):
+    queryset = Drink.objects.all()
+    serializer_class = DrinkSerializer
+
+
+class DrinkDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Drink.objects.all()
+    serializer_class = DrinkSerializer
