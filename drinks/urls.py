b55dd4431b24079975e3ce07d15ccbae4c5c7526
Phares <abungaphares@gmail.com>
2017-04-25 04:56:54 +0300

    @api_view for drinks view functions

--- drinks/urls.py
+++ drinks/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
 from drinks import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
+
     url(r'^drinks/$', views.drink_list),
     url(r'^drinks/(?P<pk>[0-9]+)/$', views.drink_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- drinks/views.py
+++ drinks/views.py
@@ -7,49 +7,51 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 from drinks.models import Drink
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from drinks.serializers import DrinkSerializer
 
 # Create your views here.
-@csrf_exempt
-def drink_list(request):
+
+@api_view(['GET', 'POST'])
+def drink_list(request, format=None):
     """
-    List all drinks, or create a new drink.
+    List all drinks, or create a new snippet.
     """
     if request.method == 'GET':
         drinks = Drink.objects.all()
         serializer = DrinkSerializer(drinks, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = DrinkSerializer(data=data)
+        serializer = DrinkSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
-def drink_detail(request, pk):
+@api_view(['GET', 'PUT', 'DELETE'])
+def drink_detail(request, pk, format=None):
     """
-    Retrieve, update or delete a drink.
+    Retrieve, update or delete a snippet instance.
     """
     try:
         drink = Drink.objects.get(pk=pk)
     except Drink.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = DrinkSerializer(drink)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = DrinkSerializer(drink, data=data)
+        serializer = DrinkSerializer(drink, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         drink.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
