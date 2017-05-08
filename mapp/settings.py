f90b971268d9a8bd836a886847cf91d51de70bb5
Phares <abungaphares@gmail.com>
2017-05-08 17:17:04 +0300

    collect static

--- mapp/settings.py
+++ mapp/settings.py
@@ -127,11 +127,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/'),
+    os.path.join(BASE_DIR, 'staticfiles/'),
 )
 
 APPEND_SLASH = True
