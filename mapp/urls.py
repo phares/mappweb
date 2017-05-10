057e0ad0a5c156134b3f16e57101c2e0aa8e24d1
Phares <abungaphares@gmail.com>
2017-05-10 16:45:18 +0300

    add urlpattern for static files

--- mapp/urls.py
+++ mapp/urls.py
@@ -16,6 +16,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^', include('products.urls')),
@@ -23,7 +24,7 @@ urlpatterns = [
     url(r'^', include('address.urls')),
     url(r'^', include('customers.urls')),
     url(r'^admin/', include(admin.site.urls)),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += [
     url(r'^api-auth/', include('rest_framework.urls',
