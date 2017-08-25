fa27675357fc6afd51fdaf589132dbf315e815ea
phares <abungaphares@gmail.com>
2017-08-25 22:48:49 +0300

    products and product categories image blank false

--- products/models.py
+++ products/models.py
@@ -12,19 +12,19 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=100, blank=False,default='')
     owner = models.ForeignKey('auth.User', related_name='categories', on_delete=models.CASCADE)
     active = models.BooleanField(default=True)
-    image = models.ImageField(upload_to='categories', default='/images/default.png')
+    image = models.ImageField(upload_to='categories', blank=False)
 
 
 class Product(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    name = models.CharField(max_length=100, blank=False,default='')
+    name = models.CharField(max_length=100, blank=False, default='')
     price = models.DecimalField(decimal_places=0,max_digits=6)
     active = models.BooleanField(default=True)
     volume = models.IntegerField(default=0)
     quantity = models.IntegerField(default=0)
     category = models.ForeignKey('products.ProductCategory', related_name='categories', on_delete=models.CASCADE)
     owner = models.ForeignKey('auth.User', related_name='products', on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='products', default='/images/default.png')
+    image = models.ImageField(upload_to='products', blank=False)
 
     class Meta:
         ordering = ('created',)
